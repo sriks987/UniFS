1a82847784ea6b755b3056a3d65764996713193c
SRIKUMAR S <srik8552@gmail.com>
2018-10-29 00:23:53 +0530

    Function declarations

--- fs.c
+++ fs.c
@@ -37,6 +37,8 @@ struct memInode{
 	unsigned char flagBlock; // Flag for modified block 
 };
 
+// For the flags 0 - not occupied, 1 - occupied
+
 struct inodeBit{
 	unsigned char flag[NUM_INODES];
 };
@@ -45,7 +47,29 @@ struct dataBit{
 	unsigned char flag[NUM_BLOCKS];
 };
 
-// System call definitions 
+// System call declarations
 
+static int mkdir_f(const char *path, mode_t fileMode);
+static int readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
+static int getattr_f(const char *path, struct stat *st);
+static int rmdir_f(const char *path);
+static int open_f(const char *path, struct fuse_file_info *fi);
+static int create_f(const char *path, mode_t md, struct fuse_file_info *fi);
+static int read_f(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi);
+static int write_f(const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *fi);
+static int unlink_f(const char *path);
+
+
+static struct fuse_operations operations = {
+	.mkdir = mkdir_f,
+	.readdir = readdir_f,
+	.getattr = getattr_f,
+	.rmdir = rmdir_f,
+	.open = open_f,
+	.create = create_f,
+	.write = write_f,
+	.read = read_f,
+	.unlink = unlink_f
+};
 
 
